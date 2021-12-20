ca06d7b7063fe18b7188788f5cad76e6b7ce6ef5
Cho-soo-ah <83620662+Cho-soo-ah@users.noreply.github.com>
2021-12-21 00:37:48 +0900

    Add files via upload

--- main/js/canvas.js
+++ main/js/canvas.js
@@ -1,101 +1,114 @@
-// let canvas;
-// let ctx;
-// let flowField;
-// let flowFieldAnimation;
-// window.onload = function(){
-//     canvas = document.getElementById('canvas');
-//     ctx = canvas.getContext('2d');
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-//     flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-//     flowField.animate(0);
-// }
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particleArray = [];
 
-// window.addEventListener('resize',function(){
-//     this.cancelAnimationFrame(flowFieldAnimation);
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-//     flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-//     flowField.animate(0);
-// });
+const mouse ={
+    x:null,
+    y:null,
+    radius: 100
+}
 
-// const mouse = {
-//     x: 0,
-//     y: 0,
-// }
-// window.addEventListener('mousemove',function(e){
-//     mouse.x = e.x;
-//     mouse.y = e.y;
-// })
+window.addEventListener('mousemove', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
 
-// class FlowFieldEffect{
-//     #ctx;
-//     #width;
-//     #height;
-//     constructor(ctx,width,height){
-//         this.#ctx = ctx;
-//         // this.#ctx.strokeStyle = 'white';
-//         this.#ctx.lineWidth = 1;
-//         this.#width = width;
-//         this.#height = height;
-//         this.#drawLine(10, 10);
-//         // this.x = 0;
-//         // this.y = 0;
-//         // this.angle = 0;
-//         this.lastTime = 0;
-//         this.interval = 1000/60;
-//         this.timer = 0;
-//         this.cellSize = 15;
-//         this.gradient;
-//         this.#createGradient();
-//         this.#ctx.strokeStyle = this.gradient;
-//         this.radius =  5;
-//         this.vr = 0.03;
-//     }
-//     #createGradient(){
-//         this.gradient = this.#ctx.createLinearGradient(0,0,this.#width, this.#height);
-//         this.gradient.addColorStop('0.1','#ccccff');
-//         this.gradient.addColorStop('0.6','#b3ffff');
-//         this.gradient.addColorStop('0.9','#80ff80');
-//     }
-//     #drawLine(angle, x, y){
-//         let positionX = x;
-//         let positionY = y;
-//         let dx = mouse.x - positionX;
-//         let dy = mouse.y - positionY;
-//         let distance = dx*dx + dy*dy;
-//         if(distance > 300000) distance = 300000;
-//         else if(distance < 5000) distance = 5000;
+ctx.fillStyle = 'teal';
+ctx.font = '60px Verdana';
+ctx.fillText('SVG', 35, 70);
+// ctx.strokeRect(0,0,100,100);
+const textCoordinates = ctx.getImageData(0,0, canvas.width, canvas.height);
 
-//         let length = distance/10000;
-//         this.#ctx.beginPath();
-//         this.#ctx.moveTo(x, y);
-//         this.#ctx.lineTo(x + Math.cos(angle) * length,  y + Math.sin(angle) * length);
-//         this.#ctx.stroke();
-//     }
-//     animate(timeStamp){
-//         const deltaTime = timeStamp - this.lastTime;
-//         this.lastTime = timeStamp;
-//         if(this.timer > this.interval ){
-//             // this.angle += 0.1;
-//             this.#ctx.clearRect(0,0,this.#width, this.#height);
-//             this.radius += this.vr;
-//             if(this.radius > 5 || this.radius < -5) this.vr *= -1;
+class particle{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.size = 3;
+        this.baseX = this.x;
+        this.baseY = this.y;
+        this.density = (Math.random() * 30) + 1;
+    }
+    draw(){
+        ctx.fillStyle = 'teal';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
+        ctx.closePath();
+        ctx.fill();
+    }
+    update(){
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx*dx + dy*dy);
+        let forceDirectionX = dx/ distance;
+        let forceDirectionY = dy/ distance;
+        let maxDistance = mouse.radius;
+        let force = (maxDistance - distance) / maxDistance;
+        let directionX = forceDirectionX * force * this.density;
+        let directionY = forceDirectionY * force * this.density;
+        if(distance< mouse.radius){
+            this.x -= directionX *3;
+            this.y -= directionY *3;
+        }else{
+            if(this.x !== this.baseX){
+                let dx = this.x - this.baseX;
+                this.x -= dx/5;
+            }
+            if(this.y !== this.baseY){
+                let dy = this.y - this.baseY;
+                this.y -= dy/5;
+            }
+        }
+    }
+}
+console.log(textCoordinates);
+function init(){
+    particleArray = [];
+    // for(let i=0; i<500; i++){
+    //     let x = Math.random()* canvas.width;
+    //     let y = Math.random()* canvas.height;
+    //     particleArray.push(new particle(x,y));
+    // }
+    for(let y =0, y2= textCoordinates.height;y < y2; y++){
+        for(let x =0, x2 = textCoordinates.width; x<x2;x++){
+            if(textCoordinates.data[(y * 4 * textCoordinates.width)+(x*4)+3]>128){
+                let positionX = x;
+                let positionY = y; 
+                particleArray.push(new particle(positionX *10, positionY * 10));
+            }
+        }
+    }
+}
+init();
+console.log(particleArray);
 
-//             for(let y = 0; y < this.#height; y += this.cellSize){
-//                 for(let x= 0; x < this.#width; x += this.cellSize){
-//                     const angle = (Math.cos(mouse.x *0.001) + Math.sin(mouse.y*0.001))* this.radius;
-//                     this.#drawLine(angle, x, y);
-//                 }
-//             }
-//             this.timer = 0;
-//         }else {
-//             this.timer += deltaTime;
-//         }
-//         // this.#draw(this.#width/2 + Math.sin(this.angle) * 100, this.#height/2 + Math.cos(this.angle) * 100);
-//         // this.x += 0.5;
-//         // this.y += 2.5;
-//         // console.log("animate");
-//         flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
-//     }
-// }
+function animate(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    for(let i=0;i<particleArray.length; i++){
+        particleArray[i].draw();
+        particleArray[i].update();
+    }
+    connect();
+    requestAnimationFrame(animate);
+}
+animate();
+
+function connect(){
+    for(let a=0; a<particleArray.length;a++){
+        for(let b= a; b< particleArray.length;b++){            
+            let dx = particleArray[a].x - particleArray[b].x;
+            let dy = particleArray[a].y - particleArray[b].y;
+            let distance = Math.sqrt(dx*dx + dy*dy);
+
+            if(distance<15){
+                ctx.strokeStyle = 'teal';
+                ctx.lineWidth = 2;
+                ctx.beginPath();
+                ctx.moveTo(particleArray[a].x, particleArray[a].y);
+                ctx.lineTo(particleArray[b].x, particleArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
