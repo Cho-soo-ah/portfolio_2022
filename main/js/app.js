4b59ed4794c41a6efc7c15e5f8774fb66afcb17c
Cho-soo-ah <83620662+Cho-soo-ah@users.noreply.github.com>
2021-12-20 23:05:39 +0900

    Add files via upload

--- main/js/app.js
+++ main/js/app.js
@@ -1,3 +1,4 @@
+// up animation
 function isElementUnderBottom(elem, triggerDiff) {
     const { top } = elem.getBoundingClientRect();
     const { innerHeight } = window;
@@ -9,7 +10,7 @@ function isElementUnderBottom(elem, triggerDiff) {
     elems.forEach(elem => {
       if (isElementUnderBottom(elem, -30)) {
         elem.style.opacity = "0";
-        elem.style.transform = 'translateY(50px)';
+        elem.style.transform = 'translateY(200px)';
   
       } else {
         elem.style.opacity = "1";
@@ -21,6 +22,52 @@ function isElementUnderBottom(elem, triggerDiff) {
   
   window.addEventListener('scroll', handleScroll);
 
-  var myPath = document.querySelector(".cls-2");
-  var length = myPath.getTotalLength();
-  console.log(length);
+// bllearn section
+function random(min, max) { //min~max 범위
+  //toFixed()를 통해 반환된 문자 데이터를,
+  //parseFloat()을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floating(item, delayAfter, size) {
+    gsap.to(
+        item, //선택자
+        random(0.8, 1.3), //1.5~2.5초 사이의 랜덤한 애니메이션 동작 시간
+        {
+            delay: random(0, delayAfter), //몇초 뒤에 애니메이션을 실행, 지연 시간 설정
+            y: size, //transform: translateY(수치)와 같음. 수직으로 움직이는 크기
+            repeat: -1, //몇 번 반복하는지를 설정, -1은 무한 반복.
+            yoyo: true, //한번 재생된 애니메이션을 다시 뒤로 재생-위아래 움직임
+            ease: Power1.easeInOut //easing 함수 적용
+        }
+    );
+}
+
+floating('.con', .2, 60)
+
+// menu
+const m1 = document.querySelector('.m1');
+const m2 = document.querySelector('.m2');
+const m3 = document.querySelector('.m3');
+const m4 = document.querySelector('.m4');
+const bllearnTop = document.querySelector('#bllearn_sec').offsetTop;
+const designTop = document.querySelector('#design_sec').offsetTop;
+const turtleTop = document.querySelector('#turtle_sec').offsetTop;
+
+m1.addEventListener("click",svgHandle);
+m2.addEventListener("click",bllearnHandle);
+m3.addEventListener("click",designHandle);
+m4.addEventListener("click",turtleHandle);
+
+function svgHandle(){
+  window.scrollTo({top:0, behavior:"smooth"});
+}
+function bllearnHandle(){
+  window.scrollTo({top:bllearnTop, behavior:"smooth"});
+}
+function designHandle(){
+  window.scrollTo({top:designTop, behavior:"smooth"});
+}
+function turtleHandle(){
+  window.scrollTo({top:turtleTop, behavior:"smooth"});
+}
